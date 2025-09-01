fdfa54a3b87d1f790dae0905e849c1ac17cb22b4
somanosuke <somanosuke.suke@gmail.com>
2025-09-01 18:17:38 +0900

    ヘッダーコンポネント作成

---
+++ laravel/resources/js/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Icon from "./svgs/DocumentSvg.vue";
+</script>
+
+<template>
+  <div class="component-root border-b border-solid border-yellow-200 shadow-md bg-white">
+    <div class="text pt-8 p-6">
+      <div class="upper flex justify-center items-center gap-3">
+        <Icon />
+        <h1
+          class="text-3xl font-bold bg-gradient-to-r from-red-500 from- to-orange-500 to- bg-clip-text text-transparent"
+        >
+          かんたんメモ
+        </h1>
+      </div>
+      <div class="lower text-center text-gray-600 text-sm mt-2">
+        <p>思いついたことをサッと記録しよう</p>
+      </div>
+    </div>
+  </div>
+</template>
--- laravel/resources/js/pages/app.vue
+++ laravel/resources/js/pages/app.vue
@@ -1,1 +1,9 @@
-<template>hello,world</template>
+<script setup lang="ts">
+import Header from "../components/Header.vue";
+</script>
+
+<template>
+  <div class="page-root bg-primary-50 pb-20">
+    <Header />
+  </div>
+</template>
