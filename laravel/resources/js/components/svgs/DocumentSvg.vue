8bfcfb54e4aa7f059da2b77eb6eeede497b67f5e
somanosuke <somanosuke.suke@gmail.com>
2025-09-04 12:36:55 +0900

    メモ0件のときのUI作成

--- laravel/resources/js/components/svgs/DocumentSvg.vue
+++ laravel/resources/js/components/svgs/DocumentSvg.vue
@@ -1,15 +1,13 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="32"
-    height="32"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="lucide lucide-sticky-note text-primary-500"
+    class="lucide lucide-sticky-note w-full h-full"
   >
     <path d="M16 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V8Z"></path>
     <path d="M15 3v4a2 2 0 0 0 2 2h4"></path>
